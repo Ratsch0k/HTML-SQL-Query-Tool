d62c5becde470bd37f360b9484f8779fe59bdbf0
Simon Kurz <simonlukas.98@gmail.com>
2019-05-11 02:14:07 +0200

    Starting testing layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col></Col>
+        <Col>
+          <AlertClosable />
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col>
+          <p>Here should be displayed text</p>
+          <hr />
+          <p>Here should be more text</p>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
+}
+
+class AlertClosable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { show: true };
+  }
+
+  render() {
+    const handleClose = () => this.setState({ show: false });
+
+    return (
+    <Alert show={this.state.show}  variant='warning' dismissible onClose={handleClose}>
+      <Alert.Heading>Only For Development</Alert.Heading>
+      This is only a test site for testing architecture currently used in development
+    </Alert>
+  );
+  }
 }
 
 export default App;
