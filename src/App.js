dd38f91c0c2c73e14e441ce0d6b7a9ec1d2b5058
Simon Kurz <simonlukas.98@gmail.com>
2019-05-14 23:10:37 +0200

    Corrected onChange
    - correct spelling mistake off postgresql
    - changd input to form group
    - added onSubmit functon
    - binding input to state
    - added onChange function for input

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <Container fluid>
-      <h1 id='heading'><strong>PostgrSQL Query Tool</strong></h1>
+      <h1 id='heading'><strong>PostgreSQL Query Tool</strong></h1>
       <Row>
         <Col lg={3} md={1} xs={0}></Col>
         <Col lg={6} md={10}>
@@ -29,33 +29,46 @@ class QueryInput extends React.Component {
     super(props, context);
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       size: 'lg',
       first: true,
       className:'mb-3 mt-5',
+      value: '',
     };
   }
 
-  handleSubmit(){
-    if(this.state.first){
-      this.setState({size:'sm', first: false, className: 'mb-1 mt-1'});
+  handleSubmit(event){
+    event.preventDefault();
+
+    if(this.state.first) {
+      this.setState({size: '', first: false, className: 'mb-1 mt-1'});
     }
+
+    const axios = require('axios');
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+    return false;
   }
 
   render() {
     return(
-      <div>
+      <Form onSubmit={this.handleSubmit}>
         <InputGroup className={this.state.className} size={this.state.size} id='query-input'>
           <Form.Control id='query-command'
-            placeholder='PostgrSQL Query Command'
-            aria-describedby='query-button'/>
+            placeholder='PostgreSQL Query Command'
+            aria-describedby='query-button'
+            value={this.state.value}
+            onChange={this.handleChange}/>
           <InputGroup.Append>
             <Button variant='outline-success'
-              onClick={this.handleSubmit} type="submit">Submit</Button>
+              onClick={this.handleSubmit} type="button">Submit</Button>
           </InputGroup.Append>
         </InputGroup>
-      </div>
+      </Form>
     );
   }
 }
